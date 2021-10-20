7b00ea8a7ee0ad00c82166d1e43f9924130f1c85
StevenZambito <zambito.steven@icloud.com>
2021-10-20 10:53:21 -0400

    Created the "new" and "create" actions in the Albums controller, as well as a private "album_params" action.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,18 @@
 class AlbumsController < ApplicationController
     def index
     end
+
+    def new
+        @album = Album.new
+    end
+
+    def create
+        @album = Album.new(album_params)
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:title, :description)
+    end
 end
