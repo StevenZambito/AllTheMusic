476f01b8329758608e131319098f409b7d5b529a
StevenZambito <zambito.steven@icloud.com>
2021-10-20 13:50:27 -0400

    Created a before_action in the Albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,25 +1,34 @@
 class AlbumsController < ApplicationController
+    before_action :find_album, only: [:show, :edit, :update, :destroy]
+
     def index
       @albums = Album.all.order("created_at DESC")
     end
 
     def new
-        @album = Album.new
+      @album = Album.new
+    end
+
+    def show
     end
 
     def create
-        @album = Album.new(album_params)
+      @album = Album.new(album_params)
 
-        if @album.save
-            redirect_to root_path
-        else 
-            render 'new'
-        end
+      if @album.save
+        redirect_to root_path
+      else 
+        render 'new'
+      end
     end
 
     private
 
     def album_params
-        params.require(:album).permit(:title, :description, :artist)
+      params.require(:album).permit(:title, :description, :artist)
+    end
+
+    def find_album
+      @album = Album.find(params[:id])
     end
 end
