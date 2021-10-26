154055ca69d0b4839db78503b234c94742d1dc5f
StevenZambito <zambito.steven@icloud.com>
2021-10-26 15:53:48 -0400

    Generated a migration to add an album_id to the reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_194708) do
+ActiveRecord::Schema.define(version: 2021_10_26_195236) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_194708) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "album_id"
   end
 
   create_table "tracks", force: :cascade do |t|
