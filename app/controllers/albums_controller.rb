8aa67d683956fdb57797d8424c2dcb3a7a530d23
StevenZambito <zambito.steven@icloud.com>
2021-10-27 14:29:04 -0400

    Wrote a conditional in Albums controller's show action to check if thhere is an average rating and if there is, to then assign that rating to an instance variable

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -17,6 +17,12 @@ class AlbumsController < ApplicationController
     end
 
     def show
+      if @album.reviews.blank?
+        @average_review = 0
+      else
+        @average_review = @album.reviews.average(:rating).round(2)
+      end
+
     end
 
     def create
