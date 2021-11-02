90a74fd078e3c0e9e9962e1242c7efc288995883
StevenZambito <zambito.steven@icloud.com>
2021-11-02 11:31:41 -0400

    Generated a migration to add a column called "title" to the Reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_141749) do
+ActiveRecord::Schema.define(version: 2021_11_02_153028) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_141749) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "album_id"
+    t.string "title"
   end
 
   create_table "tracks", force: :cascade do |t|
