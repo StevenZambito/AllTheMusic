33e4c255be5874bb1945a8bfbda6731d62992931
StevenZambito <zambito.steven@icloud.com>
2021-11-02 10:18:44 -0400

    Added "Release Date" column to Albums table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_134417) do
+ActiveRecord::Schema.define(version: 2021_11_02_141749) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_134417) do
     t.integer "user_id"
     t.integer "genre_id"
     t.string "label"
+    t.date "release_date"
   end
 
   create_table "genres", force: :cascade do |t|
